9175c349fd2063076db592472b166a426ec5e3a3
Lucas Ament <lucasament5@gmail.com>
2021-03-14 11:24:30 -0300

    Class File em andamento

--- src/main/File.java
+++ src/main/File.java
@@ -1,5 +1,17 @@
+import java.io.*;
 package main;
 
-public class File {
 
+public class File{
+	   BufferedReader in = null;
+	   try {
+		   in = new BufferedReader(new FileReader("C:\\Users\\Usuario\\git\\maze-solver\\File\\File.Example.txt"));
+		   String str;
+		   while((str = in.readLine()) != null) {
+			   System.out.println(str);
+		   }
+			in.close();   
+	   }catch(IOException e) {
+		   
+	   }
 }
--- src/main/Main.java
+++ src/main/Main.java
@@ -4,26 +4,27 @@ public class Main
 {
 
 	public static void main(String[] args) {
-	    try {
-            Stack st = new Stack(2);  
-            
-            st.push(1); // Adiciona 1
-            return ret;
-	    }
-            System.out.println(st.isEmpty()); // false
-            System.out.println(st.pop()); // 1
-            System.out.println(st.isEmpty()); // true
-            
-            st.push(1); // Adiciona 1
-            st.push(2); // Adiciona 2
-            
-            System.out.println(st.getTop()); // 2
-            
-            st.push(3); // Erro, sem espaço
-            
-	    } catch(Exception e) {
-            System.err.println(e);
-	    }
+		 try {
+	            Stack st = new Stack(2);  
+	            
+	            st.push(1); // Adiciona 1
+	            return ret;
+		    }
+	            System.out.println(st.isEmpty()); // false
+	            System.out.println(st.pop()); // 1
+	            System.out.println(st.isEmpty()); // true
+	            
+	            st.push(1); // Adiciona 1
+	            st.push(2); // Adiciona 2
+	            
+	            System.out.println(st.getTop()); // 2
+	            
+	            st.push(3); // Erro, sem espaço
+	            
+		    } catch(Exception e) {
+	            System.err.println(e);
+		    }
 
 	}
-}
+ }
+
