887f87fd291e91b76d71d1335cf6c78ee2df7aad
Guilherme Araujo Alexandre <69830140+Guilherme-A-A@users.noreply.github.com>
2021-04-21 16:16:40 -0300

    Implementacao do JavaDoc!

--- src/main/Coordinates.java
+++ src/main/Coordinates.java
@@ -1,5 +1,18 @@
 package main;
 
+ /**
+  * Classe que demonstra as coordenadas a serem percorridas no labirinto.
+  * 
+  * @param coordinate       	Coordenada inteira que sera transformada em 2 coordenadas- x e y
+  * @param getCoordinate    	Obtem as coordenadas que serao percorridas
+  * @param getX             	Obtem as coordenadas de X percorridas
+  * @param getY             	Obtem as coordenadas de Y percorridas
+  * @param updateCoordinates    Altera o caminho que foi percorrido no labirinto
+  * @return                     Retorna todas as coordenadas obtidas
+  * @return                     Retorna as coordenadas obtidas de X em 0
+  * @return                     Retorna as coordenadas obtidas de Y em 1
+  */
+
 public class Coordinates {
 	private int coordinate[] = new int[2];
 	
