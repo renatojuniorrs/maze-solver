9115f7fb04b232850234b538047d4a50d0430e00
Guilherme Araujo Alexandre <69830140+Guilherme-A-A@users.noreply.github.com>
2021-04-23 22:17:47 -0300

    Implementação Javadoc na classe Positions.java

--- src/main/Positions.java
+++ src/main/Positions.java
@@ -1,5 +1,12 @@
 package main;
 
+/**
+ * Class that shows all the positions that "Bob" can go through in the maze
+ *
+ * @param Positions   Declares positions traversed in the maze
+ * @return            void 
+ */
+
 public enum Positions {
 	TOP,
 	BOTTOM,
