1a49cabb11c48b3a4e41d4c309c1fc53eaef4804
Guilherme Araujo Alexandre <69830140+Guilherme-A-A@users.noreply.github.com>
2021-04-23 22:37:08 -0300

    Javadoc no File.java
    Acho que faltou algumas coisas!

--- src/main/File.java
+++ src/main/File.java
@@ -2,6 +2,12 @@ package main;
 
 import java.io.*;
 
+ /**
+  * Declares and reads a string stack, checking if it is empty, if not add a line in the maze, through a Class Constructor.
+  * 
+  * @return    Return FileContent after reading the file          
+  */
+
 public class File {
 	private Stack<String> FileContent = new Stack<String>();
 
